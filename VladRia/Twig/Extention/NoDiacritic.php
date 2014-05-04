5c86ce5a6eb1c8864b88a646f725627496af6536
vladria <valdria@mail.ru>
2014-05-04 22:20:35 +0200

    5

---
+++ VladRia/Twig/Extention/NoDiacritic.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace VladRia\Twig\Extention;
+
+/**
+* A terribly useful class
+*
+* See: http://knpuniversity.com/screencast/question-answer-day
+*/
+class NoDiacritic
+{
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
